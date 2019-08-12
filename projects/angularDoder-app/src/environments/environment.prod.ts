e32af94fbf16d1517dfe30ac964b883b781d9ff9
mati3780 <mati9281@hotmail.com>
2019-08-12 14:00:39 -0300

    enviroments

--- projects/angularDoder-app/src/environments/environment.prod.ts
+++ projects/angularDoder-app/src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
-  authority: "https://sso-test-angularDoder.azurewebsites.net",
-  client_id: "angularApp",
-  scopes: "openid profile roles apiGateway customersApi api1",
-  redirect_uri: "https://neuridiumtest.azureedge.net/redirect",
-  silent_redirect_uri: "https://neuridiumtest.azureedge.net/silent-renew.html",
-  post_logout_redirect_uri: "https://neuridiumtest.azureedge.net/logoutredirect",
-  silent_renew_offset_in_seconds: 20,
+ 
 
   filterProtocolClaims: true,
   loadUserInfo: true,
